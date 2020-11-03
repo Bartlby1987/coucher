ed805cc6c1ea63265c69344395b9ed9b4826fa13
bartlby1987 <bartlby1987@gmail.com>
2020-11-03 12:52:19 +0300

    change (if else) structure in co-optimus-scrapper.js and rename fileUtils.js on infoUtils.js.

--- scrapper/src/co-optimus-scrapper.js
+++ scrapper/src/co-optimus-scrapper.js
@@ -1,8 +1,8 @@
 const cheerio = require('cheerio');
 const request = require('sync-request');
+const mkdirp = require('mkdirp');
 const {logInfo, logError} = require("./utils/logUtils");
-const {isInfoExists, isCouchOrFeaturesExists} = require("./utils/fileUtils");
-
+const {isInfoExists, isCouchOrFeaturesExists} = require("./utils/infoUtils");
 const generateDataPageUrl = (value) => `https://www.co-optimus.com/ajax/ajax_games.php?game-title-filter=&system=28&countDirection=at%20least&playerCount=2&couch=true&page=${value}&sort=&sortDirection=`
 
 function getGamesInfo() {
@@ -38,50 +38,49 @@ function getGamesInfo() {
 function parseRow(gameRow) {
     let tdNameAndGenre = gameRow.childNodes[1];
     let gameName = tdNameAndGenre.children[0].children[0].data
-    if (isInfoExists(gameName)) {
-        let genre = tdNameAndGenre.children[3].children[0].data
-        if (isInfoExists((genre))) {
-            let tdCouch = gameRow.childNodes[5];
-            let couch = tdCouch.children[0].data;
-            if (isCouchOrFeaturesExists(couch)) {
-                let tdFeatures = gameRow.children[9];
-                let featuresInfoArray = tdFeatures.children;
-                if (isCouchOrFeaturesExists(featuresInfoArray)) {
-                    let features = [];
-                    for (let k = 0; k < featuresInfoArray.length; k++) {
-                        let oneFeature = featuresInfoArray[k];
-                        let attribs = oneFeature["attribs"];
-                        let featuresInfo = attribs["title"];
-                        features.push(featuresInfo);
-                    }
-                    let tdReleaseInfo = gameRow.children[15];
-                    let releaseInfo = tdReleaseInfo.children[0];
-                    let releaseDate = releaseInfo.children[0].data;
-                    if (isInfoExists(releaseDate)) {
-                        logInfo(`Parsing game (${gameName}) has been successfully completed`)
-                        return {
-                            "game": gameName,
-                            "genre": genre,
-                            "couch": couch,
-                            "features": features,
-                            "releaseDate": releaseDate
-                        };
-                    } else {
-                        logError(`Error.Release date is not exists`);
-                    }
-                } else {
-                    logError(`Error.Features not found`);
-                }
-            } else {
-                logError(`Error.Couch not found`);
-            }
-        } else {
-            logError(`Error.Genre not found`);
-        }
-    } else {
+    if (!isInfoExists(gameName)) {
         logError(`Error. Game not found`);
+        return;
     }
+    let genre = tdNameAndGenre.children[3].children[0].data
+    if (!isInfoExists((genre))) {
+        logError(`Error.Genre not found`);
+        return
+    }
+    let tdCouch = gameRow.childNodes[5];
+    let couch = tdCouch.children[0].data;
+    if (!isCouchOrFeaturesExists(couch)) {
+        logError(`Error.Couch not found`);
+        return;
+    }
+    let tdFeatures = gameRow.children[9];
+    let featuresInfoArray = tdFeatures.children;
+    if (!isCouchOrFeaturesExists(featuresInfoArray)) {
+        logError(`Error.Features not found`);
+        return
+    }
+    let features = [];
+    for (let k = 0; k < featuresInfoArray.length; k++) {
+        let oneFeature = featuresInfoArray[k];
+        let attribs = oneFeature["attribs"];
+        let featuresInfo = attribs["title"];
+        features.push(featuresInfo);
+    }
+    let tdReleaseInfo = gameRow.children[15];
+    let releaseInfo = tdReleaseInfo.children[0];
+    let releaseDate = releaseInfo.children[0].data;
+    if (!isInfoExists(releaseDate)) {
+        logError(`Error.Release date is not exists`);
+        return
+    }
+    logInfo(`Parsing game (${gameName}) has been successfully completed`)
+    return {
+        "folderName": "",
+        "gameName": gameName,
+        "genre": genre,
+        "couch": couch,
+        "features": features,
+        "releaseDate": releaseDate
+    };
 }
 
-
-console.log(getGamesInfo());
