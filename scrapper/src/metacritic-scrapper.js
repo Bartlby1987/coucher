be2774a0d253069202c13abe3c4898e1089e2d1f
bartlby1987 <bartlby1987@gmail.com>
2020-11-04 17:46:11 +0300

    Has been added logging im metacritic-scrapper.js .

--- scrapper/src/metacritic-scrapper.js
+++ scrapper/src/metacritic-scrapper.js
@@ -3,55 +3,70 @@ const request = require('sync-request');
 const fs = require("fs");
 const path = require('path');
 const {gatGamesNames} = require("./utils/infoUtils");
-const URL_SITE_PAGE = `https://www.metacritic.com/`
+const URL_SITE_PAGE = `https://www.metacritic.com/`;
+const logger = require('./utils/logUtils').getLogger("metacritic-checker");
 const getSearchGamePage = (gameName) => `${URL_SITE_PAGE}search/game/${gameName}/results?plats[268409]=1&search_type=advanced`;
-let GAME_PAGE_HREF_SELECTOR = "#main_content > div.fxdrow.search_results_wrapper > div.module.search_results.fxdcol.gu6 > div.body > ul > " +
+const GAME_PAGE_HREF_SELECTOR = "#main_content > div.fxdrow.search_results_wrapper > div.module.search_results.fxdcol.gu6 > div.body > ul > " +
     " li > div > div.basic_stats.has_thumbnail > div > h3 > a";
-let META_INFO_SELECTOR = `div.summary_wrap > div.section.product_scores > div.details.main_details > div > div > a > div > span`;
-let USER_INFO_SELECTOR = `div.summary_wrap > div.section.product_scores > div.details.side_details > div:nth-child(1) > div > a > div`;
+const META_INFO_SELECTOR = `div.summary_wrap > div.section.product_scores > div.details.main_details > div > div > a > div > span`;
+const USER_INFO_SELECTOR = `div.summary_wrap > div.section.product_scores > div.details.side_details > div:nth-child(1) > div > a > div`;
 const pathFolder = path.resolve(`../`);
 
 function getMetacriticScore() {
-    let gamesNames = gatGamesNames();
-    for (let i = 0; i < gamesNames.length; i++) {
-        let metacriticInfo;
-        let metaScore;
-        let userScore;
-        let gameName = gamesNames[i];
-        let searchGamePage = getSearchGamePage(gameName);
-        let res = request('GET', encodeURI(searchGamePage), {});
-        let htmlSearchPage = cheerio.load(res.body);
-        let gamePageHref = htmlSearchPage(GAME_PAGE_HREF_SELECTOR).attr("href");
-        if (gamePageHref) {
-            gamePageHref = gamePageHref.slice(1);
-            let gamePage = `${URL_SITE_PAGE}${gamePageHref}`;
-            let gamePageRes = request('GET', encodeURI(gamePage), {});
-            let htmlGamePage = cheerio.load(gamePageRes.body);
-            let metaInfo = htmlGamePage(META_INFO_SELECTOR).text();
-            let userInfo = htmlGamePage(USER_INFO_SELECTOR).text();
-            if (metaInfo) {
-                metaScore = metaInfo;
-            } else {
-                metaScore = "NA";
-            }
-            if (!!userInfo && userInfo !== "tbd") {
-                userScore = userInfo;
-            } else {
-                userScore = "NA";
-            }
-            metacriticInfo = {
-                "gameName": gameName,
-                "metaScore": metaScore,
-                "userScore": userScore,
-            }
-        } else {
-            metacriticInfo = {
-                "gameName": gameName,
-                "error": "game not found"
-            }
+    try {
+        let gamesNames = gatGamesNames();
+        if (gamesNames.length === 0 || !gamesNames) {
+            logger.error(`Games folder not nave information`);
+            return;
         }
-        let gameInfoPath = `${pathFolder}/games/${gameName}/metacriticInfo.json`;
-        fs.writeFileSync(gameInfoPath, JSON.stringify(metacriticInfo));
+        for (let i = 0; i < gamesNames.length; i++) {
+            let metacriticInfo;
+            let metaScore;
+            let userScore;
+            let gameName = gamesNames[i];
+            let searchGamePage = getSearchGamePage(gameName);
+            let res = request('GET', encodeURI(searchGamePage), {});
+            let htmlSearchPage = cheerio.load(res.body);
+            let gamePageHref = htmlSearchPage(GAME_PAGE_HREF_SELECTOR).attr("href");
+            if (gamePageHref) {
+                gamePageHref = gamePageHref.slice(1);
+                let gamePage = `${URL_SITE_PAGE}${gamePageHref}`;
+                let gamePageRes = request('GET', encodeURI(gamePage), {});
+                let htmlGamePage = cheerio.load(gamePageRes.body);
+                let metaInfo = htmlGamePage(META_INFO_SELECTOR).text();
+                let userInfo = htmlGamePage(USER_INFO_SELECTOR).text();
+                if (metaInfo) {
+                    logger.info(`Metascore on (${gameName}) has been successfully found: (${metaInfo})`)
+                    metaScore = metaInfo;
+                } else {
+                    logger.error(`Metascore on game ${gameName} not found.`)
+                    metaScore = "NA";
+                }
+                if (!!userInfo && userInfo !== "tbd") {
+                    logger.info(`User score on (${gameName}) has been successfully found: (${userInfo})`)
+                    userScore = userInfo;
+                } else {
+                    logger.error(`User score on game ${gameName} not found.`)
+                    userScore = "NA";
+                }
+                metacriticInfo = {
+                    "gameName": gameName,
+                    "metaScore": metaScore,
+                    "userScore": userScore,
+                }
+            } else {
+                logger.error(`Game ${gameName} not found.`)
+                metacriticInfo = {
+                    "gameName": gameName,
+                    "error": "game not found"
+                }
+            }
+            logger.info(`Creation metacriticInfo on game (${gameName}: ${metaScore};${userScore}) has been completed.`)
+            let gameInfoPath = `${pathFolder}/games/${gameName}/metacriticInfo.json`;
+            fs.writeFileSync(gameInfoPath, JSON.stringify(metacriticInfo));
+        }
+    } catch (err) {
+        logger.error("Technical error", err);
     }
 }
 
