753378f135f478f8e6acd0b38b9b86c1aa56f155
bartlby1987 <bartlby1987@gmail.com>
2020-11-04 19:13:10 +0300

    Has been changed search on game name.

--- scrapper/src/metacritic-scrapper.js
+++ scrapper/src/metacritic-scrapper.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const request = require('sync-request');
 const fs = require("fs");
 const path = require('path');
-const {gatGamesNames} = require("./utils/infoUtils");
+const {gatGamesAndFoldersNames} = require("./utils/infoUtils");
 const URL_SITE_PAGE = `https://www.metacritic.com/`;
 const logger = require('./utils/logUtils').getLogger("metacritic-checker");
 const getSearchGamePage = (gameName) => `${URL_SITE_PAGE}search/game/${gameName}/results?plats[268409]=1&search_type=advanced`;
@@ -11,19 +11,20 @@ const GAME_PAGE_HREF_SELECTOR = "#main_content > div.fxdrow.search_results_wrapp
 const META_INFO_SELECTOR = `div.summary_wrap > div.section.product_scores > div.details.main_details > div > div > a > div > span`;
 const USER_INFO_SELECTOR = `div.summary_wrap > div.section.product_scores > div.details.side_details > div:nth-child(1) > div > a > div`;
 const pathFolder = path.resolve(`../`);
-
 function getMetacriticScore() {
     try {
-        let gamesNames = gatGamesNames();
-        if (gamesNames.length === 0 || !gamesNames) {
-            logger.error(`Games folder not nave information`);
+        let gamesAndFoldersNames = gatGamesAndFoldersNames();
+        let gamesFoldersName = Object.keys(gamesAndFoldersNames);
+        if (gamesFoldersName.length === 0 || !gamesFoldersName) {
+            logger.error(`In games folder not have information`);
             return;
         }
-        for (let i = 0; i < gamesNames.length; i++) {
+        for (let i = 0; i < gamesFoldersName.length; i++) {
             let metacriticInfo;
             let metaScore;
             let userScore;
-            let gameName = gamesNames[i];
+            let gameFolderName=gamesFoldersName[i];
+            let gameName = gamesAndFoldersNames[gameFolderName];
             let searchGamePage = getSearchGamePage(gameName);
             let res = request('GET', encodeURI(searchGamePage), {});
             let htmlSearchPage = cheerio.load(res.body);
@@ -39,14 +40,14 @@ function getMetacriticScore() {
                     logger.info(`Metascore on (${gameName}) has been successfully found: (${metaInfo})`)
                     metaScore = metaInfo;
                 } else {
-                    logger.error(`Metascore on game ${gameName} not found.`)
+                    logger.error(`Metascore on game (${gameName}) not found.`)
                     metaScore = "NA";
                 }
                 if (!!userInfo && userInfo !== "tbd") {
                     logger.info(`User score on (${gameName}) has been successfully found: (${userInfo})`)
                     userScore = userInfo;
                 } else {
-                    logger.error(`User score on game ${gameName} not found.`)
+                    logger.error(`User score on game (${gameName}) not found.`)
                     userScore = "NA";
                 }
                 metacriticInfo = {
@@ -55,14 +56,14 @@ function getMetacriticScore() {
                     "userScore": userScore,
                 }
             } else {
-                logger.error(`Game ${gameName} not found.`)
+                logger.error(`Game (${gameName}) not found.`)
                 metacriticInfo = {
                     "gameName": gameName,
                     "error": "game not found"
                 }
             }
             logger.info(`Creation metacriticInfo on game (${gameName}: ${metaScore};${userScore}) has been completed.`)
-            let gameInfoPath = `${pathFolder}/games/${gameName}/metacriticInfo.json`;
+            let gameInfoPath = `${pathFolder}/games/${gameFolderName}/metacriticInfo.json`;
             fs.writeFileSync(gameInfoPath, JSON.stringify(metacriticInfo));
         }
     } catch (err) {
--- scrapper/src/utils/infoUtils.js
+++ scrapper/src/utils/infoUtils.js
@@ -9,17 +9,38 @@ function isCouchOrFeaturesExists(couch) {
     return !!couch
 }
 
-function gatGamesNames() {
-    const gamePath = path.resolve(`../games/`);
-    let gamesName = []
+// function gatGamesNames() {
+//     const gamePath = path.resolve(`../games`);
+//     let gamesNames = [];
+//     let gamesFoldersName = []
+//     fs.readdirSync(gamePath).forEach(file => {
+//         gamesFoldersName.push(file.trim());
+//     });
+//     for (let i = 0; i<gamesFoldersName.length; i++) {
+//         let gameFolderName=gamesFoldersName[i]
+//         let path=`${gamePath}/${gameFolderName}/gameInfo.json`;
+//         let gameInfo = JSON.parse(fs.readFileSync(path, 'utf8'));
+//         let gameName = gameInfo.gameName;
+//         gamesNames.push(gameName);
+//     }
+//     return {"gamesNames":gamesNames,
+//         "gamesFoldersName":gamesFoldersName
+//     }
+// }
+function gatGamesAndFoldersNames() {
+    const gamePath = path.resolve(`../games`);
+    let obj = {}
     fs.readdirSync(gamePath).forEach(file => {
-        gamesName.push(file.trim());
-    })
-    return gamesName;
+        let folderName = file.trim()
+        let path = `${gamePath}/${folderName}/gameInfo.json`;
+        let gameInfo = JSON.parse(fs.readFileSync(path, 'utf8'));
+        obj[folderName] = gameInfo.gameName;
+    });
+    return obj;
 }
 
 module.exports = {
     isInfoExists: isInfoExists,
     isCouchOrFeaturesExists: isCouchOrFeaturesExists,
-    gatGamesNames:gatGamesNames
+    gatGamesAndFoldersNames:gatGamesAndFoldersNames
 }
